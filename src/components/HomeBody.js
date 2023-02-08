639d29e97ebf5c3ca7f3d80e65b1038e3e3a6e98
Aladeen <home@HAladeen-MacBook-Air.local>
2023-02-07 17:46:01 -0800

    quick fix

--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -5,7 +5,7 @@ function HomeBody() {
         <div >
         <img src={AlPacinoImage} alt="pacino" data-aos="zoom-in"></img>
         </div>
-        <div className='mt-5 ms-5' data-aos="fade-up">
+        <div className='mt-5 ms-5' >
           <h1 className='mb-5'>Hoii!</h1>
           <h1>I'm Abdul...</h1>
           <h1>I'm a Software developer....</h1>
